fbaaa9183364a0bfb110e91ee8749511014dcfbf
MrNicei <cromtox614@gmail.com>
2026-09-13 05:35:38 +0100

    Connect Supabase to CROMTEL NETWORK

---
+++ src/supabase.js
@@ -0,0 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://iocuukawaklxwzoohhdc.supabase.co'
+
+const supabasePublishableKey = 'sb_publishable_vc4PZgf31O3oSPK3m5o8UA_pd2D-hB6
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabasePublishableKey
+)
