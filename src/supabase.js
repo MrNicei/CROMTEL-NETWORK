46cb1a9226a4a95ba1575599264103dcb416cd79
MrNicei <cromtox614@gmail.com>
2026-09-15 20:46:16 +0100

    Fix Supabase connection

--- src/supabase.js
+++ src/supabase.js
@@ -1,10 +1,10 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://iocuukawaklxwzoohhdc.supabase.co'
+const supabaseUrl = 'https://iocuukawaklxwzoohhdc.supabase.co';
 
-const supabasePublishableKey = 'sb_publishable_vc4PZgf31O3oSPK3m5o8UA_pd2D-hB6
+const supabasePublishableKey = 'sb_publishable_vc4PZgf31O3oSPK3m5o8UA_pd2D-hB6';
 
 export const supabase = createClient(
   supabaseUrl,
   supabasePublishableKey
-)
+);
